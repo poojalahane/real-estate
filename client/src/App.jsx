37cbf88b61d654075a639ed90ca78e7d85aaaa1d
poojalahane <poojalahane144@gmail.com>
2025-12-04 16:50:22 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign UP Page</h1>
+    </div>
+  );
+};
+
+export default SignUp;
